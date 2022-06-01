0d2f5f9d2bbe27882bf4b97e78c25e24d4c7c512
misheck12 <misheck1720@gmail.com>
2022-06-02 01:07:34 +0200

    Add tables

--- schema.sql
+++ schema.sql
@@ -7,8 +7,22 @@ CREATE TABLE animals (
     escape_attempts integer NOT NULL,
     neutered boolean NOT NULL,
     weight_kg decimal NOT NULL,
-    species string NULL,
+    species_id integer NOT NULL,
+    owner_id integer NOT NULL,
+    PRIMARY KEY (id)
 );
 
 ALTER TABLE animals ADD species CHAR(40);
 
+CREATE TABLE owners (
+    id integer NOT NULL,
+    full_name string NOT NULL,
+    age int NOT NULL,
+    primary key (id)
+);
+
+CREATE TABLE species (
+    id integer NOT NULL,
+    name string NOT NULL,
+    primary key (id)
+);
